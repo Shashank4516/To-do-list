ff5b6d8e6fa483614da27c95634722063b67d065
Shashank4516 <shashankmalik23156@gmail.com>
2024-09-26 01:06:43 +0530

    fourth-change

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addTodo();
+});
